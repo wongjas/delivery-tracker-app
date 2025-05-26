b3d543a5041194a2539332d3a3c76393315b2daa
Jason Wong <jwong@slack-corp.com>
2025-05-26 11:36:43 +0900

    Revert "Change message to process delivery order numbers"
    This reverts commit 31353bef8a604ba26fbfdc02b45dfc1f7038b056.

--- listeners/messages/delivery_message.py
+++ listeners/messages/delivery_message.py
@@ -4,42 +4,7 @@ from slack_bolt import BoltContext, Say
 
 def delivery_message_callback(context: BoltContext, say: Say, logger: Logger):
     try:
-        delivery_id = context["matches"][0]
-        say(
-            blocks=[
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": f"Delivery *{delivery_id}* has been submitted. Is this correct?"
-                    }
-                },
-                {
-                    "type": "actions",
-                    "elements": [
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "Correct",
-                                "emoji": True
-                            },
-                            "style": "primary",
-                            "action_id": "approve_delivery"
-                        },
-                        {
-                            "type": "button", 
-                            "text": {
-                                "type": "plain_text",
-                                "text": "Not correct",
-                                "emoji": True
-                            },
-                            "style": "danger",
-                            "action_id": "deny_delivery"
-                        }
-                    ]
-                }
-            ]
-        )
+        greeting = context["matches"][0]
+        say(f"{greeting}, how are you?")
     except Exception as e:
         logger.error(e)
