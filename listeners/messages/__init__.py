31353bef8a604ba26fbfdc02b45dfc1f7038b056
Jason Wong <jwong@slack-corp.com>
2025-05-26 11:35:53 +0900

    Change message to process delivery order numbers

--- listeners/messages/__init__.py
+++ listeners/messages/__init__.py
@@ -5,4 +5,4 @@ from .delivery_message import delivery_message_callback
 
 # To receive messages from a channel or dm your app must be a member!
 def register(app: App):
-    app.message(re.compile(r"hi|hello|hey", re.IGNORECASE))(delivery_message_callback)
+    app.message(re.compile(r"[A-Za-z]+-\d+"))(delivery_message_callback)
--- listeners/messages/delivery_message.py
+++ listeners/messages/delivery_message.py
@@ -4,7 +4,42 @@ from slack_bolt import BoltContext, Say
 
 def delivery_message_callback(context: BoltContext, say: Say, logger: Logger):
     try:
-        greeting = context["matches"][0]
-        say(f"{greeting}, how are you?")
+        delivery_id = context["matches"][0]
+        say(
+            blocks=[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f"Delivery *{delivery_id}* has been submitted. Is this correct?"
+                    }
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Correct",
+                                "emoji": True
+                            },
+                            "style": "primary",
+                            "action_id": "approve_delivery"
+                        },
+                        {
+                            "type": "button", 
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Not correct",
+                                "emoji": True
+                            },
+                            "style": "danger",
+                            "action_id": "deny_delivery"
+                        }
+                    ]
+                }
+            ]
+        )
     except Exception as e:
         logger.error(e)
